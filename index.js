3615d2eda1670e990c8be58e6f2db9da85b447c1
[GIT ME] <vertigo.himanshu@gmail.com>
2021-08-17 08:43:39 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+
+
+
+
+    fs.readFile('views/index.html', 'utf8', function(err, data) {
+        if (err)
+            console.log(err);
+        else
+            res.send(data)
+    });
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
