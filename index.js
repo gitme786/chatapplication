58dbedd7828a410a9ea80007d4733a55b235f9cd
[GIT ME] <vertigo.himanshu@gmail.com>
2021-08-17 21:33:25 +0530

    2nd commit

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 
+
+
+app.use('/static', express.static('static'))
+
+
 app.get('/', (req, res) => {
-
-
-
-
     fs.readFile('views/index.html', 'utf8', function(err, data) {
         if (err)
             console.log(err);
